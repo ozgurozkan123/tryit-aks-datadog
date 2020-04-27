03ea2a683b4d01c2fc9b0918ef688bf5e5320de6
Raksit <raksit.m@ku.th>
2020-04-27 13:57:17 +0700

    :construction_worker: changed from kube_admin_config to kube_config

--- aks/output.tf
+++ aks/output.tf
@@ -1,3 +1,3 @@
-output "aks_kube_admin_config" {
-  value = azurerm_kubernetes_cluster.aks_datadog.kube_admin_config.0
+output "aks_kube_config" {
+  value = azurerm_kubernetes_cluster.aks_datadog.kube_config.0
 }
--- providers.tf
+++ providers.tf
@@ -4,21 +4,21 @@ provider "azurerm" {
 }
 
 provider "kubernetes" {
-  host = module.aks.aks_kube_admin_config.host
+  host = module.aks.aks_kube_config.host
 
-  client_certificate     = base64encode(module.aks.aks_kube_admin_config.client_certificate)
-  client_key             = base64encode(module.aks.aks_kube_admin_config.client_certificate)
-  cluster_ca_certificate = base64encode(module.aks.aks_kube_admin_config.cluster_ca_certificate)
+  client_certificate     = base64encode(module.aks.aks_kube_config.client_certificate)
+  client_key             = base64encode(module.aks.aks_kube_config.client_certificate)
+  cluster_ca_certificate = base64encode(module.aks.aks_kube_config.cluster_ca_certificate)
   load_config_file       = false
 }
 
 provider "helm" {
   kubernetes {
-    host = module.aks.aks_kube_admin_config.host
+    host = module.aks.aks_kube_config.host
 
-    client_certificate     = base64encode(module.aks.aks_kube_admin_config.client_certificate)
-    client_key             = base64encode(module.aks.aks_kube_admin_config.client_certificate)
-    cluster_ca_certificate = base64encode(module.aks.aks_kube_admin_config.cluster_ca_certificate)
+    client_certificate     = base64encode(module.aks.aks_kube_config.client_certificate)
+    client_key             = base64encode(module.aks.aks_kube_config.client_certificate)
+    cluster_ca_certificate = base64encode(module.aks.aks_kube_config.cluster_ca_certificate)
     load_config_file       = false
   }
 }
