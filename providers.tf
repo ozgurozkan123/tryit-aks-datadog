1603fd0a5e1736978675828e33548419ac118dc0
Raksit <raksit.m@ku.th>
2020-04-27 10:56:22 +0700

    :wheel_of_dharma: added kubernetes provider based on aks admin config

--- providers.tf
+++ providers.tf
@@ -3,18 +3,27 @@ provider "azurerm" {
   features {}
 }
 
+provider "kubernetes" {
+  host = module.aks.kube_admin_config.host
+
+  client_certificate     = base64encode(module.aks.kube_admin_config.client_certificate)
+  client_key             = base64encode(module.aks.kube_admin_config.client_certificate)
+  cluster_ca_certificate = base64encode(module.aks.kube_admin_config.cluster_ca_certificate)
+  load_config_file       = false
+}
+
 provider "helm" {
   kubernetes {
     host = module.aks.kube_admin_config.host
 
-    client_certificate = base64encode(module.aks.kube_admin_config.client_certificate)
-    client_key = base64encode(module.aks.kube_admin_config.client_certificate)
+    client_certificate     = base64encode(module.aks.kube_admin_config.client_certificate)
+    client_key             = base64encode(module.aks.kube_admin_config.client_certificate)
     cluster_ca_certificate = base64encode(module.aks.kube_admin_config.cluster_ca_certificate)
-    load_config_file = false
+    load_config_file       = false
   }
 }
 
 data "helm_repository" "stable" {
   name = "stable"
-  url = "https://kubernetes-charts.storage.googleapis.com"
+  url  = "https://kubernetes-charts.storage.googleapis.com"
 }
