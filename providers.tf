c715e8174e9aaf76c467010f8bcbb90226a32cac
Raksit <raksit.m@ku.th>
2020-04-27 14:09:16 +0700

    :wheel_of_dharma: fixed typo client_key in kubernetes and helm provider

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ provider "kubernetes" {
   host = module.aks.aks_kube_config.host
 
   client_certificate     = base64encode(module.aks.aks_kube_config.client_certificate)
-  client_key             = base64encode(module.aks.aks_kube_config.client_certificate)
+  client_key             = base64encode(module.aks.aks_kube_config.client_key)
   cluster_ca_certificate = base64encode(module.aks.aks_kube_config.cluster_ca_certificate)
   load_config_file       = false
 }
@@ -17,7 +17,7 @@ provider "helm" {
     host = module.aks.aks_kube_config.host
 
     client_certificate     = base64encode(module.aks.aks_kube_config.client_certificate)
-    client_key             = base64encode(module.aks.aks_kube_config.client_certificate)
+    client_key             = base64encode(module.aks.aks_kube_config.client_key)
     cluster_ca_certificate = base64encode(module.aks.aks_kube_config.cluster_ca_certificate)
     load_config_file       = false
   }
