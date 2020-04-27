c87c4505a6d0ed146d4fb633f7b9ffd8b0aed49b
Raksit <raksit.m@ku.th>
2020-04-27 10:50:19 +0700

    :wheel_of_dharma: added helm provider based on aks admin config

--- providers.tf
+++ providers.tf
@@ -2,3 +2,19 @@ provider "azurerm" {
   version = "=2.7.0"
   features {}
 }
+
+provider "helm" {
+  kubernetes {
+    host = module.aks.kube_admin_config.host
+
+    client_certificate = base64encode(module.aks.kube_admin_config.client_certificate)
+    client_key = base64encode(module.aks.kube_admin_config.client_certificate)
+    cluster_ca_certificate = base64encode(module.aks.kube_admin_config.cluster_ca_certificate)
+    load_config_file = false
+  }
+}
+
+data "helm_repository" "stable" {
+  name = "stable"
+  url = "https://kubernetes-charts.storage.googleapis.com"
+}
