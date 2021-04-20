b19d5faad0e7eb3a044390504b133d4b4ec7c3b7
Özgür Özkan <ozgurozkan123@gmail.com>
2021-04-20 06:58:04 +0300

    changed CPU type and node_count because of limits

--- providers.tf
+++ providers.tf
@@ -1,3 +1,6 @@
+terraform{
+  required_version = "~> 0.14.7"
+}
 provider "azurerm" {
   version = "=2.7.0"
   features {}
@@ -10,7 +13,7 @@ provider "kubernetes" {
   client_key             = base64decode(module.aks.aks_kube_config.client_key)
   cluster_ca_certificate = base64decode(module.aks.aks_kube_config.cluster_ca_certificate)
   load_config_file       = false
-  version                = "=1.10.0"
+  version                = "=2.0.2"
 }
 
 provider "helm" {
@@ -20,9 +23,16 @@ provider "helm" {
     client_certificate     = base64decode(module.aks.aks_kube_config.client_certificate)
     client_key             = base64decode(module.aks.aks_kube_config.client_key)
     cluster_ca_certificate = base64decode(module.aks.aks_kube_config.cluster_ca_certificate)
-  
-   
+
+
   }
- 
-   version                = "=1.3.1"
+
+   version                = "=2.0.2"
 }
+
+provider "datadog" {
+  source  = "datadog/datadog"
+  version = "~> 2.22.0"
+}
+
+
